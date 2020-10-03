32fa1e7fe09cdcaf89ce1e6e662a47a049038aff
oddlyspaced <oddlyspaced1@gmail.com>
2020-10-04 04:17:59 +0530

    home: Setup Hello World

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello World"),
+    );
   }
 }
