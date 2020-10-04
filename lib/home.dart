72581ecde861ebfc0e479e1ad6ffc1fc71f76d41
oddlyspaced <oddlyspaced1@gmail.com>
2020-10-04 23:46:15 +0530

    home: Setup responsive page design with breakpoints

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -10,17 +11,37 @@ class Home extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      home: HomeWidget(),
+      home: ScreenTypeLayout(
+        breakpoints: ScreenBreakpoints(
+          desktop: 1200,
+          tablet: 1000,
+          watch: null,
+        ),
+        mobile: HomeWidgetMobile(),
+        desktop: HomeWidgetDesktop(),
+      ),
     );
   }
 }
 
-class HomeWidget extends StatefulWidget {
+class HomeWidgetMobile extends StatefulWidget {
   @override
-  _HomeWidgetState createState() => _HomeWidgetState();
+  _HomeWidgetMobileState createState() => _HomeWidgetMobileState();
 }
 
-class _HomeWidgetState extends State<HomeWidget> {
+class _HomeWidgetMobileState extends State<HomeWidgetMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class HomeWidgetDesktop extends StatefulWidget {
+  @override
+  _HomeWidgetDesktopState createState() => _HomeWidgetDesktopState();
+}
+
+class _HomeWidgetDesktopState extends State<HomeWidgetDesktop> {
   @override
   Widget build(BuildContext context) {
     var topRightTextSize = 24.0;
@@ -79,39 +100,103 @@ class _HomeWidgetState extends State<HomeWidget> {
             ),
             Expanded(
               child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Text(
-                          "I'm ",
-                          style: TextStyle(
-                            fontSize: 64.0,
+                  child: Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Text(
+                            "I'm ",
+                            style: TextStyle(
+                              fontSize: 64.0,
+                            ),
                           ),
-                        ),
-                        Text(
-                          "Hardik Srivastava",
-                          style: TextStyle(
-                            fontSize: 64.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                    Text(
-                      "Android App Developer, Flutter Web Developer\nand a 2nd Year B.Tech Student.",
-                      style: TextStyle(
-                        fontSize: 36.0,
-                        fontWeight: FontWeight.w100,
+                          Text(
+                            "Hardik Srivastava",
+                            style: TextStyle(
+                              fontSize: 64.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              ),
+                      Text(
+                        "Android App Developer, Flutter Web Developer\nand a 2nd Year B.Tech Student.",
+                        style: TextStyle(
+                          fontSize: 36.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Container(
+                    height: 240,
+                    width: 240,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.network(
+                        "https://avatars1.githubusercontent.com/u/47493184?s=460&u=21462bee9328a42d555c4611e4869931c2d09446&v=4",
+                      ),
+                    ),
+                  ),
+                ],
+              )),
             ),
-            Text("hi"),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 32, 0),
+                  child: Container(
+                    height: 48,
+                    width: 48,
+                    child: Image.network(
+                      'https://github.com/oddlyspaced/oddlyspaced.github.io/raw/master/assets/github.png',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 32, 0),
+                  child: Container(
+                    height: 48,
+                    width: 48,
+                    child: Image.network(
+                      'https://github.com/oddlyspaced/oddlyspaced.github.io/raw/master/assets/linkedin.png',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 32, 0),
+                  child: Container(
+                    height: 48,
+                    width: 48,
+                    child: Image.network(
+                      'https://github.com/oddlyspaced/oddlyspaced.github.io/raw/master/assets/medium.png',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 32, 0),
+                  child: Container(
+                    height: 48,
+                    width: 48,
+                    child: Image.network(
+                      'https://github.com/oddlyspaced/oddlyspaced.github.io/raw/master/assets/hackerrank.png',
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "srivastavahardik@gmail.com",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
